5798e25b1bae72730a8d4c3cffd0ca9a478715fe
Jasterix <jasterix19@gmail.com>
2022-05-02 18:22:26 -0400

    Handle form text

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import CourseForm from "./CourseForm";
+
 const ManageCoursePage = (props) => {
+	const [course, setCourse] = useState({
+		id: null,
+		slug: "",
+		title: "",
+		authorId: null,
+		category: "",
+	});
+
+	function handleChange({ target }) {
+		setCourse({
+			...course,
+			[target.name]: target.value, //[vent.tar...]=> computed property
+		});
+	}
 	return (
 		<>
 			<h2>Manage Course</h2>
-			<CourseForm />
+			<CourseForm course={course} onChange={handleChange} />
 		</>
 	);
 };
