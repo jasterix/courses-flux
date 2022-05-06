d53ddc2e7b58eb9d975c046a1bec211ec6eb8cf6
Jasterix <jasterix19@gmail.com>
2022-05-06 12:55:51 -0400

    Update course and home pages

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = (props) => {
+	const [errors, setErrors] = useState({});
 	const [course, setCourse] = useState({
 		id: null,
 		slug: "",
@@ -20,7 +22,22 @@ const ManageCoursePage = (props) => {
 
 	function handleSubmit(event) {
 		event.preventDefault();
-		courseApi.saveCourse(course);
+		courseApi.saveCourse(course).then(() => {
+			props.history.push("/courses");
+			toast.success("Course added!");
+		});
+	}
+
+	function formIsValid() {
+		const _errors = {};
+
+		if (!course.title) _errors.title = "Title is required";
+		if (!course.authorId) _errors.authorId = "Author ID is required";
+		if (!course.category) _errors.category = "Category is required";
+
+		setErrors(_errors);
+
+		return Object.keys(_errors).length === 0;
 	}
 
 	return (
